4359fcc9471d0d03b54d518b9aac810ca36c4384
mdenizturkmen <dnz.trkmn64@gmail.com>
2022-03-25 02:47:55 +0300

    Authentication functions moved to Authentication.js, verification state showed on screen

--- src/screens/home/profileScreen.js
+++ src/screens/home/profileScreen.js
@@ -16,8 +16,7 @@ import FireBaseContext from '../../context/fireBaseProvider';
 import {firebase} from '@react-native-firebase/auth';
 import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons';
 import {launchImageLibrary} from 'react-native-image-picker';
-import {showDangerPopup, showSuccessPopup} from '../../services/popup';
-import {Popup} from 'popup-ui';
+import {onChangePasswordPress} from '../../services/authentication';
 
 function ProfileScreen({navigation}) {
   const {logoutUser} = useContext(FireBaseContext);
@@ -25,6 +24,7 @@ function ProfileScreen({navigation}) {
   const [currentPassword, setCurrentPassword] = React.useState('');
   const [newPassword, setNewPassword] = React.useState('');
   const [photo, setPhoto] = React.useState(null);
+  const [verificationState, setVerificationState] = React.useState(false);
 
   const initials = user.displayName
     ? user.displayName
@@ -56,43 +56,9 @@ function ProfileScreen({navigation}) {
     }
   };
 
-  const reauthenticate = () => {
-    const cred = firebase.auth.EmailAuthProvider.credential(
-      user.email,
-      currentPassword,
-    );
-    return user.reauthenticateWithCredential(cred);
-  };
-
-  // Changes user's password...
-  const onChangePasswordPress = () => {
-    reauthenticate()
-      .then(() => {
-        user
-          .updatePassword(newPassword)
-          .then(() => {
-            showSuccessPopup({
-              Popup,
-              title: 'Password changed succesfully',
-              textBody: '',
-            });
-          })
-          .catch(error => {
-            showDangerPopup({
-              Popup,
-              title: 'Password change failed',
-              textBody: error.message,
-            });
-          });
-      })
-      .catch(error => {
-        showDangerPopup({
-          Popup,
-          title: 'Password change failed',
-          textBody: error.message,
-        });
-      });
-  };
+  function verifyButtonPressed() {
+    setVerificationState(!verificationState);
+  }
 
   return (
     <DefaultBackground>
@@ -110,11 +76,35 @@ function ProfileScreen({navigation}) {
         </View>
         <Title>{user.displayName}</Title>
         <Subheading>{user.email}</Subheading>
-        <Button
-          onPress={() => logoutUser({navigation})}
-          style={styles.signoutButton}>
-          Sign Out
-        </Button>
+        <Button onPress={() => logoutUser({navigation})}>Sign Out</Button>
+        {verificationState ? (
+          <View style={styles.verifiedView}>
+            <Button
+              onPress={verifyButtonPressed}
+              style={styles.verifyButton}
+              color={'#04d72f'}
+              labelStyle={styles.verifyButtonLabel}>
+              Verified
+            </Button>
+            <MaterialCommunityIcons name="check" style={styles.checkIcon} />
+          </View>
+        ) : (
+          <View style={styles.verifiedView}>
+            <Button
+              onPress={verifyButtonPressed}
+              style={styles.verifyButton}
+              color={'#f54242'}
+              labelStyle={styles.verifyButtonLabel}>
+              Verify Profile
+            </Button>
+            <MaterialCommunityIcons
+              name="exclamation-thick"
+              size={28}
+              style={styles.exclamationIcon}
+            />
+          </View>
+        )}
+
         <TouchableHighlight style={styles.addGoogleAccountButton}>
           <View style={styles.addGoogleAccountButtonView}>
             <Text style={styles.addGoogleAccountText}>Add Google Account</Text>
@@ -143,7 +133,9 @@ function ProfileScreen({navigation}) {
         />
         <TouchableHighlight
           style={styles.resetPasswordButton}
-          onPress={onChangePasswordPress}>
+          onPress={() =>
+            onChangePasswordPress(user, newPassword, currentPassword)
+          }>
           <View style={styles.resetPasswordButtonView}>
             <Text style={styles.resetPasswordButtonText}>Reset Password</Text>
           </View>
@@ -163,19 +155,52 @@ const styles = StyleSheet.create({
     alignItems: 'flex-start',
     backgroundColor: 'white',
     color: '#000000',
-    paddingBottom: 20,
   },
   container: {
     alignItems: 'center',
     backgroundColor: 'white',
   },
   avatar: {
-    marginTop: 10,
+    marginTop: 20,
   },
   signoutButton: {
     fontSize: 20,
     fontWeight: '500',
   },
+
+  verifyButton: {
+    marginRight: -5,
+  },
+
+  verifyButtonLabel: {
+    fontSize: 14,
+    fontWeight: '600',
+  },
+
+  verifiedView: {
+    flexDirection: 'row',
+    alignContent: 'center',
+    justifyContent: 'center',
+    borderRadius: 15,
+    padding: 2,
+  },
+
+  checkIcon: {
+    alignSelf: 'center',
+    color: '#ffffff',
+    fontSize: 28,
+    backgroundColor: '#65d200',
+    borderRadius: 14,
+  },
+
+  exclamationIcon: {
+    alignSelf: 'center',
+    color: '#ffffff',
+    fontSize: 25,
+    backgroundColor: '#de0101',
+    borderRadius: 14,
+  },
+
   addGoogleAccountButton: {
     flexDirection: 'column',
   },
@@ -188,8 +213,8 @@ const styles = StyleSheet.create({
     width: 250,
     alignItems: 'center',
     justifyContent: 'center',
-    marginTop: 50,
-    marginBottom: 40,
+    marginTop: 30,
+    marginBottom: 30,
   },
 
   addGoogleAccountText: {
