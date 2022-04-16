0d7e1a7bc957736347b239694c96c75db797e1e9
Carnagie <Facethest0rm!>
2022-04-16 21:17:14 +0300

    remove logging

--- src/screens/home/homeScreen.js
+++ src/screens/home/homeScreen.js
@@ -35,7 +35,6 @@ const renderListItem = ({item, navigation}) => (
 function HomeScreen({navigation}) {
   const {user} = useContext(FireBaseContext);
 
-  console.log(user);
   const [allGroupData, setAllGroup] = useState([]);
   const [classData, setClassData] = useState([]);
   const [communityData, setCommunityData] = useState([]);
